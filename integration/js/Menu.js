5988f90e60afadc7c9558e6135cd879f747f306f
NathalieTournon <ntournon@gmail.com>
2023-01-08 16:24:22 +0100

    Dynamisation Menu au scroll

--- integration/js/Menu.js
+++ integration/js/Menu.js
@@ -5,19 +5,31 @@ let Menu = class Menu {
     constructor() {
         this.getMenu();
         // Ajouter la classe top-bar--sticky à mon menu
-        this.addStickyToMenu();
+        //this.addStickyToMenu();
+        window.addEventListener('scroll', () => {
+            this.toggleStickyMenu();
+        })
     }
 
     getMenu() {
         this.menuElement = document.querySelector('.header top-bar');
     }
 
-    addStickyToMenu() {
+    toggleStickyMenu() {
+        // Récupération de la height de mon menu
+        const menuHeight = document.querySelector('.top-bar').offsetHeight;
+        console.log(menuHeight, window.scrollY);
+        // Si la distance entre le sommet de la page et le viewport est supérieure 
+        // ou égale à la height de mon menu
+        if(window.scrollY >= menuHeight) {
+            this.menuElement.classList.add('top-bar--sticky');
+        } else {
+            this.menuElement.classList.remove('top-bar--sticky');
+        }
         // Ajouter la classe top-bar--sticky à mon menu
-        this.menuElement.classList.add('top-bar--sticky');
+        //this.menuElement.classList.add('top-bar--sticky');
     }
 
-    
 };
 
 export default Menu;
